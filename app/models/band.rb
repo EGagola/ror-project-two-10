8b3a3c13987d2db257983278176f93383c646652
Elliot Gagola <egagola@spartaglobal.com>
2018-11-12 13:47:20 +0000

    V.2.1 Added validation

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,4 +1,5 @@
 class Band < ApplicationRecord
+  validates :name, :src, presence: true
   belongs_to :user
   has_many :songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ApplicationRecord
+  validates :title, :album, presence: true
   belongs_to :band
 end
